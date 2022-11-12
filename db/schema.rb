ba974e4c823f12a42cd202456305b8e9eb66247e
Rich Kaht <adamselene1776@protonmail.com>
2022-11-11 20:34:04 -0700

    Add Name column to bulk_discounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_12_012208) do
+ActiveRecord::Schema.define(version: 2022_11_12_024622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_11_12_012208) do
     t.decimal "discount_percentage", precision: 5, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
   end
 
