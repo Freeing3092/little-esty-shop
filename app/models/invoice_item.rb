3aa3b3d3e4f1e76690fb602539330ee2d19c9c41
Rich Kaht <adamselene1776@protonmail.com>
2022-11-14 15:09:08 -0700

    Feat/Test: Add InvoiceItem model method for best_available_discount

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,10 +1,18 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+  has_one :merchant, through: :item
+  has_many :bulk_discounts, through: :merchant
 
   enum status: [ :pending, :packaged, :shipped ]
 
   def item_name
     item.name
+  end
+  
+  def best_available_discount
+    bulk_discounts.where('bulk_discounts.minimum_item_quantity <= ?', self.quantity)
+    .order(discount_percentage: :desc)
+    .limit(1).first
   end
 end
