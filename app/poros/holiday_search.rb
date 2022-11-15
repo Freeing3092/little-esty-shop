09a877cfcf71d10bbd1f7eb7d30b2185dd9c91c4
Rich Kaht <adamselene1776@protonmail.com>
2022-11-14 20:00:14 -0700

    Feat: Add Holiday API call

---
+++ app/poros/holiday_search.rb
@@ -0,0 +1,14 @@
+require './app/poros/holiday'
+require './app/services/holiday_services'
+
+class HolidaySearch
+  def holiday_information
+    service.holidays.map do |data|
+      Holiday.new(data)
+    end
+  end
+  
+  def service
+    HolidayService.new
+  end
+end
