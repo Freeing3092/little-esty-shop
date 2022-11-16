63877b3f3d237760073751cc332366ca71326349
Rich Kaht <adamselene1776@protonmail.com>
2022-11-15 20:05:01 -0700

    Un-comment github API calls for heroku deployement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@
 require './app/poros/holiday_search'
 
 class ApplicationController < ActionController::Base
-  # before_action :get_info, only: %i[index show new edit]
+  before_action :get_info, only: %i[index show new edit]
   before_action :get_holidays, only: [:index]
-  # 
-  # def get_info
-  #   @repo_name = GithubSearch.new.repo_information
-  #   @contributors = GithubSearch.new.contributor_names
-  #   @pull_requests = GithubSearch.new.pull_requests
-  # end
+  
+  def get_info
+    @repo_name = GithubSearch.new.repo_information
+    @contributors = GithubSearch.new.contributor_names
+    @pull_requests = GithubSearch.new.pull_requests
+  end
   
   def get_holidays
     @upcoming_holidays = HolidaySearch.new.holiday_information
